5366bf9cb541aced7370b852badd88443b149a93
Kevin P. Fleming <kevin@km6g.us>
2018-10-28 15:51:46 -0400

    Bump version: 0.2.0 → 1.0.0

--- certbot_freenas_deployer/__init__.py
+++ certbot_freenas_deployer/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Kevin P. Fleming"""
 __email__ = 'kevin+py@km6g.us'
-__version__ = '0.2.0'
+__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kpfleming/certbot_freenas_deployer',
-    version='0.2.0',
+    version='1.0.0',
     zip_safe=False,
 )
